f18c4f23ec0b4a1fd2a0845ea96cf02f32e79517
이민규 <manq@imingyuui-MacBook-Pro.local>
2019-05-12 08:29:46 +0900

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RestangularModule, Restangular } from 'ngx-restangular';
+
+// Function for setting the default restangular configuration
+export function RestangularConfigFactory(RestangularProvider) {
+  RestangularProvider.setBaseUrl('http://www.opinet.co.kr/api/');
+  RestangularProvider.setDefaultHeaders({ 'code': 'F530190510' });
+}
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, RestangularModule.forRoot(RestangularConfigFactory)],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +28,4 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { Restangular } from 'ngx-restangular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+
+  constructor(private rest: Restangular) {
+
+  }
+
+  getGasStations() {
+    console.log("get gas-station list");
+    this.rest.all('areaCode.do?code=F530190510&out=json&area=06').getList().subscribe((results) => {
+
+    });
+  }
+
+  ngOnInit() {
+    console.log("tab1 page start!");
+    this.getGasStations();
+  }
+}
